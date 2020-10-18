c657d874c9cd4860667a1eedba449c30d88a4ae0
a <a>
2020-10-18 23:48:15 +0300

    0.0.6

--- xontrib/back2dir.py
+++ xontrib/back2dir.py
@@ -8,15 +8,12 @@ _file = Path(__xonsh__.env.get('XDG_CACHE_HOME', '~/.cache')).expanduser() / 'xo
 if not _file.parent.exists():
     os.mkdir(_file.parent)
 
-def _new_dir(newdir):
-    with open(_file, 'w') as f:
-        print(newdir, end='', file=f)
-
-@events.on_chdir
-def autojump_add_to_database(olddir, newdir, **kwargs):
-    _new_dir(newdir)
-
 if __xonsh__.env["PWD"] == __xonsh__.env["HOME"] and _file.exists():
     with open(_file) as f:
         newdir = f.read()
     dirstack.cd([newdir])
+
+@events.on_chdir
+def autojump_add_to_database(olddir, newdir, **kwargs):
+    with open(_file, 'w') as f:
+        print(newdir, end='', file=f)
