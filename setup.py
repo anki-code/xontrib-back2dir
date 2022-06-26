cc8506a44e8b4fa0106fafdd2361fd45d7c9890c
Andy Kipp <anki-code@users.noreply.github.com>
2022-06-27 00:01:39 +0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except (IOError, OSError):
 
 setuptools.setup(
     name='xontrib-back2dir',
-    version='0.1.1',
+    version='0.1.2',
     license='MIT',
     author='anki-code',
     author_email='anki-code@no.no',
