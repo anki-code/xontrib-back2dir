4aa8becd34fc20db2134b69e8bd16b194e65ff1b
Andy Kipp <anki-code@users.noreply.github.com>
2022-06-27 00:01:06 +0400

    Update back2dir.py

--- xontrib/back2dir.py
+++ xontrib/back2dir.py
@@ -1,10 +1,7 @@
-import os
-
 from pathlib import Path
 from xonsh import dirstack
 
 _file = Path(__xonsh__.env.get('XDG_CACHE_HOME', '~/.cache')).expanduser() / 'xontrib-back2dir' / 'latest.path'
-
 
 @events.on_post_init
 def on_post_init(**_):
@@ -18,6 +15,6 @@ def on_post_init(**_):
 @events.on_chdir
 def _xontrib_back2dir(olddir, newdir, **kwargs):
     if not _file.parent.exists():
-        os.mkdir(_file.parent)
+        _file.parent.mkdir(parents=True, exist_ok=True)
     with open(_file, 'w') as f:
         print(newdir, end='', file=f)
